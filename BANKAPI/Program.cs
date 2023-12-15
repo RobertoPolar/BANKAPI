039009659e21e62039018856df093ec287e75cdf
Roberto Polar Bravo <rpolar@csticorp.biz>
2023-12-15 13:43:17 -0500

    adicionando o contexto e interfaces no Program

--- BANKAPI/Program.cs
+++ BANKAPI/Program.cs
@@ -1,3 +1,6 @@
+using BANKAPI.Data;
+using BANKAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BankContext>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
